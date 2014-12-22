2d5c38ccbd988e58dacc238151945fbd832a0b4d
Thomas Chilinski <Thomas.Chilinski@gmail.com>
2014-12-22 13:49:28 -0800

    First commit

---
+++ ooo_test.py
@@ -0,0 +1,44 @@
+import random
+from urllib import urlopen
+import sys
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORDS = []
+
+PHRASES = {
+    "class %%%(%%%)":
+        "Make a class named %%% that is-a %%%.",
+    "class %%%(object): \n\tdef __init__(self, ***)":
+        "class %%% has-a __init__ that takes self and *** parameters.",
+    "class %%%(object):\n\tdef ***(self, @@@)" :
+        "class %%% has-a function named *** that takes self and @@@ parameter",
+    "*** = %%%()":
+        "Set *** to an instance of class %%%.",
+    "***.***(@@@)":
+        "From *** get the function, and call it with parameters self, @@@",
+    "***.*** = '***'":
+        "From *** get the attribute and set it to '***'."
+}
+
+# do they want to drill pharses first
+if len(sys.argv) == 2 and sys.argv[1] == "english":
+    PHRASE_FIRST = True
+else:
+    PHRASE_FIRST = False
+    
+# load up the words form the website
+for word in urlopen(WORD_URL).readlines():
+    WORDS.append(word.strip())
+    
+def convert(snippet, phrase):
+    class_names = [w.capitalize() for w in
+                random.sample(
+    
+
+    
+        
+ 
+        
+        
+    
+    
